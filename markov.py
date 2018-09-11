a379d215710f0dc906f22734256ea574b992ac8f
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-11 16:31:36 -0700

    Allow n size n-gram

--- markov.py
+++ markov.py
@@ -17,7 +17,7 @@ def open_and_read_file(file_path):
     return file_string
 
 
-def make_chains(text_string):
+def make_chains(text_string, n):
     """Take input text as string; return dictionary of Markov chains.
 
     A chain will be a key that consists of a tuple of (word1, word2)
@@ -45,36 +45,49 @@ def make_chains(text_string):
     chains = {}
 
     words_list = text_string.split()
-    for i in range(len(words_list) - 2):
-        key_tuple = (words_list[i], words_list[i+1])
+    for i in range(len(words_list)-n):
+        key_lst = []
+        for j in  range(i, i+n):
+            key_lst.append(words_list[j])
+        key_tuple = tuple(key_lst)
 
        
         if key_tuple not in chains:
             chains[key_tuple] = []
-        chains[key_tuple].append(words_list[i+2])
+        chains[key_tuple].append(words_list[i+n])
 
 
     return chains
 
 
-def make_text(chains):
+def make_text(chains, n):
     """Return text from chains."""
 
     words = []
 
     next_key = choice(list(chains.keys()))
 
-    words.append(next_key[0])
-    words.append(next_key[1])
+    for i in range(n):
+        words.append(next_key[i])
 
     while next_key in chains:
         random_value = choice(chains[next_key])
         words.append(random_value)
-        next_key = (next_key[1],random_value)
+        next_key_lst = list(next_key)[1:]
+        next_key_lst.append(random_value)
+        next_key = tuple(next_key_lst)
 
-    
 
     return " ".join(words)
+
+def input_number():
+    while True:
+        user_number = input("Enter size of n-gram: ")
+        if user_number.isdigit() and user_number != '0':
+            n = int(user_number)
+            return n
+        else:
+            print("Incorrect input. Try again.")
 
 
 if len(sys.argv) == 1:
@@ -86,10 +99,12 @@ else:
 # Open the file and turn it into one long string
 input_text = open_and_read_file(input_path)
 
+n = input_number()
+
 # Get a Markov chain
-chains = make_chains(input_text)
+chains = make_chains(input_text, n)
 
 # Produce random text
-random_text = make_text(chains)
+random_text = make_text(chains, n)
 
 print(random_text)
